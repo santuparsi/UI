62fa194470700ffa7ec596d0f7257bccb23898f0
sandotnet <87011674+sandotnet@users.noreply.github.com>
2021-07-14 13:03:29 +0530

    commit_714

--- JavaScript/HandsOn/12.js
+++ JavaScript/HandsOn/12.js
@@ -1,5 +1,8 @@
 let d=new Date();
-console.log(d);
+console.log(d.toDateString());
+console.log(d.toLocaleString());
+console.log(d.toLocaleDateString());
+console.log(d.toString('MM-dd-yy'))
 // let d1=new Date(2020,11,12);
 // console.log(d1);
 console.log(d.getFullYear());
---
+++ JavaScript/HandsOn/HTML DOM/lib.js
@@ -0,0 +1,9 @@
+function Add(a,b)
+{
+    let result=a+b;
+    document.write(`Addition:${result}`)
+}
+Greet=(n)=>
+{
+    document.getElementById('s1').innerHTML='Hello '+n;
+}
